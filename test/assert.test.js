15cd1e8148057c98f2e31c8132b56bd010119074
rexk <myungsunrex@gmail.com>
2015-05-31 23:26:21 -0700

    :white_check_mark: Add test case for assertNotRender

--- test/assert.test.js
+++ test/assert.test.js
@@ -3,18 +3,37 @@ describe('@assert', function () {
   var MochaMix = require('../');
   var ProfileLink = require('../examples/src/components/ProfileLink');
 
-  it('should assert with 3 arguments', function () {
-    MochaMix.assertRender(
-      ProfileLink,
-      { username: 'rexk' },
-      'https://www.facebook.com/rexk'
-    );
+  describe('@assertRender', function () {
+    it('should assert with 3 arguments', function () {
+      MochaMix.assertRender(
+        ProfileLink,
+        { username: 'rexk' },
+        'https://www.facebook.com/rexk'
+      );
+    });
+
+    it('should assert with 2 arguments', function () {
+      MochaMix.assertRender(
+        <ProfileLink username='rexk' />,
+        'https://www.facebook.com/rexk'
+      );
+    })
   });
 
-  it('should assert with 2 arguments', function () {
-    MochaMix.assertRender(
-      <ProfileLink username='rexk' />,
-      'https://www.facebook.com/rexk'
-    );
+  describe('@assertNotRender', function () {
+    it('should assert with 3 arguments', function () {
+      MochaMix.assertNotRender(
+        ProfileLink,
+        { username: 'rexk' },
+        'https://www.facebook.com/othername'
+      );
+    });
+
+    it('should assert with 2 arguments', function () {
+      MochaMix.assertNotRender(
+        <ProfileLink username='rexk' />,
+        'https://www.facebook.com/othername'
+      );
+    })
   });
 });
